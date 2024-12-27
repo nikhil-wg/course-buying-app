b96a1f80fe64ea4d53c5e58ba20f537801871c60
nikhil <nikwagh1609@gmail.com>
2024-12-28 00:41:15 +0530

    Your commit message

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+const app = express()
+app.use()
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+app.post("/api/v1/contant", (req, res) => {
+
+})
+app.get("/api/v1/content", (req, res) => {
+
+})
+app.get("/api/v1/purchase", (req, res) => {
+
+})
